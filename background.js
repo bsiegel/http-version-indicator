f858539a8c61a9525623f6c88d9328bfa42942a6
Brandon Siegel <brandon@smartercode.net>
2021-03-21 16:50:28 -0700

    Add HTTP/3 support

--- background.js
+++ background.js
@@ -1,38 +1,48 @@
 const STATE_NONE = "none";
 const STATE_MIXED = "mixed";
 const HEADER_SPDY = "x-firefox-spdy";
+const HEADER_HTTP3 = "x-firefox-http3";
 const RESOURCE_TYPE_MAIN_FRAME = "main_frame";
 const RESOURCE_TYPE_SUB_FRAME = "sub_frame";
 
 const state = {};
 
-function evaluateState(tabId, resourceType, header) {
+function evaluateState(tabId, resourceType, headerName, headerValue) {
   if (resourceType === RESOURCE_TYPE_MAIN_FRAME) {
-    updateState(tabId, getVersion(header));
-  } else if (state[tabId] === STATE_NONE && getVersion(header) !== STATE_NONE) {
+    updateState(tabId, getVersion(headerName, headerValue));
+  } else if (
+    state[tabId] === STATE_NONE &&
+    getVersion(headerName, headerValue) !== STATE_NONE
+  ) {
     updateState(tabId, STATE_MIXED);
   }
 }
 
-function getVersion(header) {
-  if (header === STATE_NONE) {
-    return STATE_NONE;
-  } else if (header.match(/^h2/)) {
-    return "HTTP/2";
-  } else if (header === "3.1") {
-    return "SPDY 3.1";
-  } else if (header === "3") {
-    return "SPDY 3";
-  } else if (header === "2") {
-    return "SPDY 2";
-  } else {
-    return "SPDY";
+function getVersion(headerName, headerValue) {
+  if (headerName === HEADER_HTTP3) {
+    return "HTTP/3 (" + headerValue + ")";
   }
+
+  if (headerName === HEADER_SPDY) {
+    if (headerValue.match(/^h2/)) {
+      return "HTTP/2 (" + headerValue + ")";
+    } else if (headerValue === "3.1") {
+      return "SPDY (v3.1)";
+    } else if (headerValue === "3") {
+      return "SPDY (v3)";
+    } else if (headerValue === "2") {
+      return "SPDY (v2)";
+    } else {
+      return "SPDY";
+    }
+  }
+
+  return STATE_NONE;
 }
 
 function updateState(tabId, version) {
   state[tabId] = version;
-  setPageAction(tabId, version);
+  setPageAction(tabId);
 }
 
 function setPageAction(tabId) {
@@ -47,11 +57,11 @@ function setPageAction(tabId) {
     browser.pageAction.show(tabId);
     browser.pageAction.setIcon({
       tabId: tabId,
-      path: getIcon(version),
+      path: getIcon(version)
     });
     browser.pageAction.setTitle({
       tabId: tabId,
-      title: getTitle(version),
+      title: getTitle(version)
     });
   }
 }
@@ -59,11 +69,14 @@ function setPageAction(tabId) {
 function getIcon(version) {
   if (version === STATE_MIXED) {
     return "icons/icon-gray.svg";
-  } else if (version === "HTTP/2") {
+  } else if (version.startsWith("HTTP/3")) {
+    return "icons/icon-orange.svg";
+  } else if (version.startsWith("HTTP/2")) {
     return "icons/icon-blue.svg";
-  } else {
+  } else if (version.startsWith("SPDY")) {
     return "icons/icon-green.svg";
   }
+  return null;
 }
 
 function getTitle(version) {
@@ -75,7 +88,7 @@ function getTitle(version) {
 }
 
 browser.webRequest.onHeadersReceived.addListener(
-  (e) => {
+  e => {
     if (
       e.tabId === -1 ||
       (e.type !== RESOURCE_TYPE_MAIN_FRAME &&
@@ -85,28 +98,28 @@ browser.webRequest.onHeadersReceived.addListener(
     }
 
     for (let header of e.responseHeaders) {
-      if (header.name.toLowerCase() === HEADER_SPDY) {
-        evaluateState(e.tabId, e.type, header.value);
+      let headerName = header.name.toLowerCase();
+      if (headerName === HEADER_SPDY || headerName === HEADER_HTTP3) {
+        evaluateState(e.tabId, e.type, headerName, header.value);
         return;
       }
     }
-
-    evaluateState(e.tabId, e.type, STATE_NONE);
+    evaluateState(e.tabId, e.type);
   },
   { urls: ["<all_urls>"] },
   ["responseHeaders"]
 );
 
-browser.webNavigation.onCommitted.addListener((e) => {
+browser.webNavigation.onCommitted.addListener(e => {
   if (e.frameId === 0) {
     setPageAction(e.tabId);
   }
 });
 
-browser.tabs.onActivated.addListener((e) => {
+browser.tabs.onActivated.addListener(e => {
   setPageAction(e.tabId);
 });
 
-browser.tabs.onRemoved.addListener((tabId) => {
+browser.tabs.onRemoved.addListener(tabId => {
   state[tabId] = null;
 });
