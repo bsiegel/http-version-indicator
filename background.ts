cb5a2639a5ec0b00dbf5cce28b3c60c4fba242fe
Brandon Siegel <brandon@smartercode.net>
2021-04-27 00:02:18 -0700

    Drastically simplify state tracking

--- background.ts
+++ background.ts
@@ -1,115 +1,72 @@
-const STATE_NONE = "none";
-const STATE_MIXED = "mixed";
 const HEADER_SPDY = "x-firefox-spdy";
 const HEADER_HTTP3 = "x-firefox-http3";
 const RESOURCE_TYPE_MAIN_FRAME = "main_frame";
-const RESOURCE_TYPE_SUB_FRAME = "sub_frame";
 
-const state: { [key: number]: string } = {};
+type HttpProtocol = { name: string; version?: string };
 
-function evaluateState(
-  tabId: number,
-  resourceType: browser.webRequest.ResourceType,
-  headerName: string,
-  headerValue: string
-) {
-  if (resourceType === RESOURCE_TYPE_MAIN_FRAME) {
-    updateState(tabId, getVersion(headerName, headerValue));
-  } else if (
-    state[tabId] === STATE_NONE &&
-    getVersion(headerName, headerValue) !== STATE_NONE
-  ) {
-    updateState(tabId, STATE_MIXED);
-  }
-}
+const state: { [key: number]: HttpProtocol | undefined } = {};
 
-function getVersion(headerName: string, headerValue: string): string {
-  if (headerName === HEADER_HTTP3) {
-    return "HTTP/3 (" + headerValue + ")";
-  }
-
-  if (headerName === HEADER_SPDY) {
-    if (headerValue.match(/^h2/)) {
-      return "HTTP/2 (" + headerValue + ")";
-    } else if (headerValue === "3.1") {
-      return "SPDY (v3.1)";
-    } else if (headerValue === "3") {
-      return "SPDY (v3)";
-    } else if (headerValue === "2") {
-      return "SPDY (v2)";
-    } else {
-      return "SPDY";
-    }
-  }
-
-  return STATE_NONE;
-}
-
-function updateState(tabId: number, version: string) {
-  state[tabId] = version;
+function setState(tabId: number, protocol: HttpProtocol) {
+  state[tabId] = protocol;
   setPageAction(tabId);
 }
 
-function setPageAction(tabId: number) {
-  const version = state[tabId];
-  if (!version) {
-    return;
+function getHttp2Protocol(headerValue?: string): HttpProtocol {
+  if (headerValue && headerValue.match(/^h2/)) {
+    return { name: "HTTP/2" };
+  } else if (headerValue === "3.1") {
+    return { name: "SPDY", version: "3.1" };
+  } else if (headerValue === "3") {
+    return { name: "SPDY", version: "3" };
+  } else if (headerValue === "2") {
+    return { name: "SPDY", version: "2" };
+  } else {
+    return { name: "SPDY" };
   }
+}
 
-  if (version === STATE_NONE) {
+function setPageAction(tabId: number) {
+  const protocol = state[tabId];
+
+  if (!protocol) {
     browser.pageAction.hide(tabId);
   } else {
     browser.pageAction.show(tabId);
-    browser.pageAction.setIcon({
-      tabId: tabId,
-      path: getIcon(version),
-    });
     browser.pageAction.setTitle({
       tabId: tabId,
-      title: getTitle(version),
+      title: getTitle(protocol),
     });
   }
 }
 
-function getIcon(version: string): string {
-  if (version === STATE_MIXED) {
-    return "icons/icon-gray.svg";
-  } else if (version.startsWith("HTTP/3")) {
-    return "icons/icon-orange.svg";
-  } else if (version.startsWith("HTTP/2")) {
-    return "icons/icon-blue.svg";
-  } else if (version.startsWith("SPDY")) {
-    return "icons/icon-green.svg";
+function getTitle(protocol: HttpProtocol): string {
+  let version = protocol.name;
+  if (protocol.version) {
+    version += " (" + protocol.version + ")";
   }
-  return null;
-}
 
-function getTitle(version: string): string {
-  if (version === STATE_MIXED) {
-    return browser.i18n.getMessage("pageActionTitleMixed");
-  } else {
-    return browser.i18n.getMessage("pageActionTitle", version);
-  }
+  return browser.i18n.getMessage("pageActionTitle", version);
 }
 
 browser.webRequest.onHeadersReceived.addListener(
   (e) => {
-    if (
-      e.tabId === -1 ||
-      (e.type !== RESOURCE_TYPE_MAIN_FRAME &&
-        e.type !== RESOURCE_TYPE_SUB_FRAME)
-    ) {
+    if (e.tabId === -1 || e.type !== RESOURCE_TYPE_MAIN_FRAME) {
       return;
     }
 
-    for (const header of e.responseHeaders) {
-      const headerName = header.name.toLowerCase();
-      if (headerName === HEADER_SPDY || headerName === HEADER_HTTP3) {
-        evaluateState(e.tabId, e.type, headerName, header.value);
-        return;
+    for (const header of e.responseHeaders || []) {
+      switch (header.name.toLowerCase()) {
+        case HEADER_HTTP3:
+          setState(e.tabId, { name: "HTTP/3", version: header.value });
+          return;
+        case HEADER_SPDY:
+          setState(e.tabId, getHttp2Protocol(header.value));
+          return;
       }
     }
-    evaluateState(e.tabId, e.type, undefined, undefined);
+
+    const statusLineVersion = e.statusLine.split(" ", 2)[0];
+    setState(e.tabId, { name: statusLineVersion });
   },
   { urls: ["<all_urls>"] },
   ["responseHeaders"]
